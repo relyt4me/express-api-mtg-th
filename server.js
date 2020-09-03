bb1128118a7a64dc6a7cc1576022cb448cc76875
Tyler Haglund <relyt4me@gmail.com>
2020-09-03 16:02:24 -0600

    Create get for individual card

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Magic Cards';
 app.locals.cards = [
   {
+    id: 1,
     title: "Gaea's Protector",
     castingCost: '4G',
     color: 'Green',
@@ -13,6 +14,7 @@ app.locals.cards = [
     textBox: "Gaea's Protector must be blacked if able. \"Fallen Phyrexians were transmuted into elementals by Gaea long ago, but Yavimaya's other inhabitants still regard them with unease",
   },
   {
+    id: 2,
     title: 'Krosan Druid',
     castingCost: '2G',
     color: 'Green',
@@ -21,6 +23,7 @@ app.locals.cards = [
     textBox: 'Kicker 4G (You may pay an additional 4G as you cast this spell.) When Krosan Druid enters the battlefield, if it was kicked, you gain 10 life. "Druids endure disaster as seeds endure winter. Now Krosa blooms one more."',
   },
   {
+    id: 3,
     title: 'Stinging Shot',
     castingCost: 'G',
     color: 'Green',
@@ -34,6 +37,19 @@ app.get('/api/v1/cards', (request, response) => {
   const cards = app.locals.cards;
 
   response.json({ cards });
+});
+
+app.get('/api/v1/cards/:id', (request, response) => {
+  const { id } = request.params;
+  const matchingCard = app.locals.cards.find((card) => {
+    return card.id === parseInt(id);
+  });
+
+  if (!matchingCard) {
+    response.status(404).json('Card not found');
+  }
+
+  response.status(200).json(matchingCard);
 });
 
 app.listen(app.get('port'), () => {
